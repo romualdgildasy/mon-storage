2e255798f06920bead026fbedc9286007f014333
Romuald <romualdgilads6@gmail.com>
2025-11-25 18:06:25 +0100

    ajustement et fin du code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { StorageService } from '../../services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  pwd = '';
 
+  // Cette propriété remplace localStorage dans le HTML
+  get isFirstTime(): boolean {
+    return !localStorage.getItem('master_password_v2');
+  }
+
+  constructor(
+    private auth: AuthService,
+    private storage: StorageService,
+    private router: Router
+  ) {}
+
+  async login() {
+    if (this.isFirstTime) {
+      if (this.pwd.length >= 4) {
+        this.auth.setPassword(this.pwd);
+        this.auth.login();
+        await this.storage.init(this.pwd);
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert('Mot de passe trop court ! Minimum 4 caractères');
+      }
+    } else {
+      if (this.auth.checkPassword(this.pwd)) {
+        this.auth.login();
+        await this.storage.init(this.pwd);
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert('Mot de passe incorrect !');
+      }
+    }
+  }
 }
